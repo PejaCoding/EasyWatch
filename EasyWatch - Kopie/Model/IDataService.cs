27614ab8e61c3ced6cb6b485cd13b1da24195286
PeJaCoding <3dokta@gmail.com>
2015-05-21 09:17:33 +0200

    Initial Push

---
+++ EasyWatch - Kopie/Model/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace EasyWatch.Model
+{
+    public interface IDataService
+    {
+        Task LadeSerien(Action<ObservableCollection<Serie>, Exception> callback);
+        Task LadeSerienInformationen(Action<SerienInformationen, Exception> callback, Serie selectedSerie);
+        Task LadeSerienInformationen(Action<SerienInformationen, Exception> callback, Serie selectedSerie, string selectedStaffel);
+        Task LadeEpisodenInformationen(Action<EpisodenInformationen, Exception> callback, Serie selectedSerie,string selectedStaffel, Epi selectedEpisode);
+    }
+}
---
+++ EasyWatch/Model/Serie.cs
@@ -0,0 +1,16 @@
+﻿namespace EasyWatch.Model
+{
+    public class Serie
+    {
+        public SerienInformationen SerienInformation { get; set; }
+        public EpisodenInformationen episodenInformationen { get; set; }
+        public string series { get; set; }
+        public string id { get; set; }
+
+        public override string ToString()
+        {
+           return series;
+            
+        }
+    }
+}
